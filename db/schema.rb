7b84044dc6e132260f2a830f50d4b11b1d34ff79
Ross Becker <ross.becker@gmail.com>
2024-11-30 13:15:54 -0800

    generate item model: rails g model item name:string description:string uuid:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_30_211527) do
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "uuid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end

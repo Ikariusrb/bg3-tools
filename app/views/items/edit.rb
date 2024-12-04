c4b35835b2ad8de580c87c35c15f7e63e2a9d4ff
Ross Becker <ross.becker@gmail.com>
2024-12-04 13:20:30 -0800

    fix submit button for edit

--- app/views/items/edit.rb
+++ app/views/items/edit.rb
@@ -1,5 +1,6 @@
 class Items::Edit < Phlex::HTML
   include Phlex::Rails::Helpers::FormFor
+  include Phlex::Rails::Helpers::LinkTo
 
   attr_reader :item
 
@@ -25,7 +26,8 @@ class Items::Edit < Phlex::HTML
       end
 
       div do
-        f.submit "Update Item"
+        f.submit("Save", class: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline")
+        link_to("Cancel", :back)
       end
     end
   end

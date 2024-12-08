637d16df1af51ae47bf56100ee26cfc6578ecddc
Ross Becker <ross.becker@gmail.com>
2024-12-08 12:02:26 -0800

    Extract Item show template to generic resource show template

--- app/frontend/view/resource/show.rb
+++ app/frontend/view/resource/show.rb
@@ -1,28 +1,28 @@
 # /home/rbecker/code/bg3-tools/app/views/items/show.rb
 
-class View::Items::Show < Phlex::HTML
+class View::Resource::Show < Phlex::HTML
   include Phlex::Rails::Helpers::LinkTo
   include Phlex::Rails::Helpers::Routes
   include Phlex::Rails::Helpers::Routes
+  include Phlex::Rails::Helpers::ControllerName
+  include Phlex::Rails::Helpers::URLFor
 
-  attr_reader :item, :notice
+  attr_reader :resource, :notice
 
-  def initialize(item, notice: nil)
-    @item = item
+  def initialize(resource, notice: nil)
+    @resource = resource
     @notice = notice
   end
 
   def view_template
     p(style: "color:#008000") { notice }
-    h1 { @item.name }
-    p { @item.description }
     ul do
-      @item.attributes.each do |key, value|
+      resource.attributes.except("id").each do |key, value|
         li { "#{key}: #{value}" }
       end
     end
-    link_to "All Items",
-            items_path,
+    link_to "All #{resource.class.name.pluralize}",
+            url_for(action: :index),
             class:
               "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
   end

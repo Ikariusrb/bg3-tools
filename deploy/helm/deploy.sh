6742a912194dd1eb6550bcf2cd5b115ead7067f4
Ross Becker <ross.becker@gmail.com>
2025-01-25 19:09:01 -0800

    closer to working config

--- deploy/helm/deploy.sh
+++ deploy/helm/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-VERSION=`git rev-parse --short=8 HEAD`;  helm upgrade bg3-tools . --install --set version=${VERSION} --set appName=bg3-tools --set environment=development
+VERSION=`git rev-parse --short=8 HEAD`;  helm upgrade bg3-tools . --install --set version=${VERSION} --set appName=bg3-tools --set environment=production

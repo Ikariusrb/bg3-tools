6b171db3f0d7f4b078822be51be034876cb1feb2
Ross Becker <ross.becker@gmail.com>
2025-03-21 12:52:12 -0700

    run annotations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  name        :string
+#  uuid        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Item < ApplicationRecord
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  name        :string
+#  uuid        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do

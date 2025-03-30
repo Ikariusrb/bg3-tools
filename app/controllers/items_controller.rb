bf885b69d4c26e8d444f91141b8c6f8f88f1ae22
Ross Becker <ross.becker@gmail.com>
2025-03-29 20:52:46 -0700

    add new permitted fields for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ResourceController
   RESOURCE = "item".freeze
   INDEX_COLUMNS = %w[name description uuid].freeze
-  PERMIT_ATTRIBUTES = %i[name description uuid].freeze
+  PERMIT_ATTRIBUTES = %i[name description item_type location act rarity uuid weight price effects].freeze
 end

4d445cfc48fe422b1fcef56949ff78f14bf3da76
Ross Becker <ross.becker@gmail.com>
2025-04-05 08:21:07 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :builds
-  resources :build_items, only: [ :create, :destroy ]
+  resources :items, :builds
+  resource :build_items, only: [ :create, :destroy ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: redirect("/builds")
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check

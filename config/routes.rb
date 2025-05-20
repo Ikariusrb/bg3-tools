d58cd8a2f70e4cde999fe2b9553356b833ea23a8
Ross Becker <ross.becker@gmail.com>
2025-05-20 10:04:18 -0700

    put in all routes for build items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :items, :builds
-  resource :build_items, only: [ :create, :destroy ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :items, :builds, :build_items
 
   root to: redirect("/builds")
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

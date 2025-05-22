bff6863ba1548aa886e01d497ecad1810031bf12
Ross Becker <ross.becker@gmail.com>
2025-05-22 09:41:28 -0700

    move the builditem picker to it's own phlex object

--- app/frontend/view/builds/edit.rb
+++ app/frontend/view/builds/edit.rb
@@ -26,35 +26,7 @@ class View::Builds::Edit < Phlex::HTML
           render View::Builds::EditForm.new(resource, action: action)
         end
 
-        h3(class: "text-lg font-semibold mb-0 mt-4") { "Build Items" }
-        form_with(
-          url: url_for(controller: "build_items", action: "create"),
-          id: "build_items",
-          class: "inline-block mt-0 pt-0",
-          format: :turbo_stream,
-          local: false,
-          data: { turbo: true, turbo_stream: true, controller: 'builds', builds_target: 'form' }
-          ) do |f|
-          input(type: "hidden", name: "build_item[build_id]", value: resource.id)
-          input(type: "hidden", name: "build_item[item_id]", id: "selected_item_id")
-          # Add items section with dropdown and add button
-          div do
-            div(class: "flex items-end space-x-2 items-center") do
-              div(class: "flex-grow") do
-                f.select(:items, Item.pluck(:name, :id), {}, { data: { controller: 'slim-select', slim_target: 'field', builds_target: 'select' }, class: "w-full" })
-              end
-
-              button(type: "submit", data: { action: "builds#addSelectedItem" }) do
-                heroicon("plus-circle", variant: :outline)
-              end
-            end
-          end
-        end
-
-        # Add table to display associated items
-        div do
-          render View::BuildItems::TableFrame.new(build: resource)
-        end
+        render View::BuildItems::SubForm.new(resource: resource, sub_resource: nil)
       end
     end
   end

7b3634d671fca493a042d1f60f949ae933b18bbe
Ross Becker <ross.becker@gmail.com>
2025-05-22 09:42:09 -0700

    improvements to the build form

--- app/frontend/view/builds/edit_form.rb
+++ app/frontend/view/builds/edit_form.rb
@@ -22,24 +22,24 @@ class View::Builds::EditForm < Phlex::HTML
   def view_template
     form_for(resource) do |f|
       resource.attributes.except(*%w[id created_at updated_at]).transform_keys(&:to_sym).each do |attribute, value|
-        div do
-          f.label attribute
+        div(class: 'space-x-2 my-2') do
+          f.label(attribute, class: 'inline-block text-right w-20')
           f.text_field attribute
         end
       end
 
-      div do
-        form_actions(f)
-      end
+      form_actions(f)
     end
   end
 
   def form_actions(form)
-    form.submit("Save", class: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline")
-    if action == :new
-      link_to("Cancel", url_for(action: :index))
-    else
-      link_to("Cancel", url_for(resource))
+    div(class: 'space-x-2 my-2') do
+      form.submit("Save", class: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline")
+      if action == :new
+        link_to("Cancel", url_for(action: :index))
+      else
+        link_to("Cancel", url_for(resource))
+      end
     end
   end
 end

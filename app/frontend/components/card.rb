fd4ea0b10c0596ec7c574e92f70b1be055347233
Ross Becker <ross.becker@gmail.com>
2025-06-19 15:06:42 -0700

    add css merging to card component

--- app/frontend/components/card.rb
+++ app/frontend/components/card.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
 class Components::Card < Components::Base
-  CARD_CSS = %w[border divide-y divide-gray-200 overflow-hidden rounded-lg shadow dark:bg-slate-850 dark:divide-slate-700].freeze
+  include CSSMerging
 
-  def initialize(title:, extra_css: [])
+  CARD_BORDER_DEFAULT_CSS = "border divide-y divide-gray-200 overflow-hidden rounded-lg shadow dark:bg-slate-850 dark:divide-slate-700"
+  CARD_TITLE_FRAME_DEFAULT_CSS = "px-4 py-5 sm:px-6 dark:bg-slate-850"
+  CARD_BODY_DEFAULT_CSS = "px-4 py-5 sm:p-6 dark:bg-slate-800"
+
+  def initialize(title:, css: {})
     @title = title
-    @extra_css = extra_css
+    @css = css
   end
 
   def view_template
-    div(class: (CARD_CSS + @extra_css).join(' ')) do
-      div(class: "px-4 py-5 sm:px-6 dark:bg-slate-850") do
+    div(class: merged_css(:card_border)) do
+      div(class: merged_css(:card_title_frame)) do
         h3(class: "card-title") { @title }
       end
-      div(class: "px-4 py-5 sm:p-6 dark:bg-slate-800") do
+      div(class: merged_css(:card_body)) do
         whitespace
         yield
       end

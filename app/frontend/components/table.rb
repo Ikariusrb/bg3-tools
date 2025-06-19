820d1ecc37c0d3f70ca78c315e321a2a762b3e0c
Ross Becker <ross.becker@gmail.com>
2025-06-18 18:35:17 -0700

    tune up table component, improve CSS

--- app/frontend/components/table.rb
+++ app/frontend/components/table.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Components::Table < Components::Base
+  DEFAULT_TABLE_CLASSES = "min-w-full divide-y divide-gray-200 dark:divide-gray-700"
+  DEFAULT_THEAD_CLASSES = "bg-gray-50 dark:bg-slate-700"
+  DEFAULT_TBODY_CLASSES = "bg-white dark:bg-slate-800"
+  DEFAULT_TH_CLASSES = "px-6 py-3 text-left text-xs font-semibold text-gray-500 dark:text-gray-200 uppercase tracking-wider"
+  DEFAULT_TR_CLASSES = "even:bg-slate-800 odd:bg-slate-600"
+  DEFAULT_TD_CLASSES = "border-b border-slate-400 whitespace-nowrap p-2 text-sm text-gray-350 dark:text-gray-350"
+
   def initialize(rows)
     @rows = rows
     @columns = []
@@ -9,18 +16,20 @@ class Components::Table < Components::Base
   def view_template(&)
     vanish(&)
 
-    table(class: "min-w-full divide-y divide-gray-200 dark:divide-gray-700") do
-      thead(class: "bg-gray-50 dark:bg-slate-700") do
-        @columns.each do |column|
-          th(class: "px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider") { column[:header] }
+    div(class: "border rounded-lg overflow-hidden my-8") do
+      table(class: DEFAULT_TABLE_CLASSES) do
+        thead(class: DEFAULT_THEAD_CLASSES) do
+          @columns.each do |column|
+            th(class: DEFAULT_TH_CLASSES) { column[:header] }
+          end
         end
-      end
 
-      tbody do
-        @rows.each do |row|
-          tr do
-            @columns.each do |column|
-              td(class: "px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300") { column[:content].call(row) }
+        tbody(class: DEFAULT_TBODY_CLASSES) do
+          @rows.each do |row|
+            tr(class: DEFAULT_TR_CLASSES) do
+              @columns.each do |column|
+                td(class: DEFAULT_TD_CLASSES) { column[:content].call(row) }
+              end
             end
           end
         end
